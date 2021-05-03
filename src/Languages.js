abf759538eebbd64190c1dd0c30123ecf6b38450
Gustavo Gomez <gomezf09@gmail.com>
2021-05-02 19:29:34 -0500

    add basic changes

---
+++ src/Languages.js
@@ -0,0 +1,20 @@
+export const LANGUAGES = [
+  'EN', 'ES'
+]
+
+export const LANGUAGES_TEXT = {
+  "ES":{
+    info:
+      {
+        intro: 'Hola, soy',
+        personalTitle: 'Desarrollador de Software'
+      }
+  },
+  "EN":{
+    info:
+      {
+        intro: 'Hello, I\'m',
+        personalTitle: 'Software Developer'
+      }
+  }
+}
