d7d9c19cf2a5eb893e606006a3752f2259cf9bb1
gustavogomez <gomezf09@gmail.com>
2024-04-30 10:19:51 -0500

    implement dark mode and update favicon and metadata

--- src/app/_sections/Header.tsx
+++ src/app/_sections/Header.tsx
@@ -1,26 +1,51 @@
+'use client'
 import {Logo} from "@/app/assets/logo";
+import Image from "next/image";
+import sunSVG from "@/app/assets/sun.svg";
+import moonSVG from "@/app/assets/moon.svg";
+import {useTheme} from "next-themes";
+
+const navOptions = [
+  {
+    name: 'Home',
+    link: '/'
+  },
+  {
+    name: 'Servicios',
+    link: '/services'
+  },
+  {
+    name: 'Portfolio',
+    link: '/portfolio'
+  },
+  {
+    name: 'Blog',
+    link: '/blog'
+  },
+]
 
 const Header = () => {
+  const {theme, setTheme} = useTheme()
 
   return (
-    <div className="w-full h-20 flex items-center justify-center px-5 fixed top-0 z-100 bg-white text-black">
+    <div
+      className="w-full h-20 flex items-center justify-center px-5 fixed top-0 z-100 bg-white dark:bg-[#12203D] text-black dark:text-white">
       <div className="justify-between flex w-[90%]">
         <Logo/>
         <nav className="flex justify-between items-center">
-          <a className='font-bold text-base leading-5 text-center mr-10 cursor-pointer ' href="/">Home</a>
-          <a className='font-bold text-base leading-5 text-center mr-10 cursor-pointer' href="/services">Servicios</a>
-          <a className='font-bold text-base leading-5 text-center mr-10 cursor-pointer' href="/portfolio">Portfolio</a>
-          <a className='font-bold text-base leading-5 text-center mr-10 cursor-pointer' href="/blog">Blog</a>
-          {/*      <li>*/}
-          {/*        <a href="/portfolio">Portfolio</a>*/}
-          {/*      </li>*/}
-          {/*      <li>*/}
-          {/*        <a href="/blog">Blog</a>*/}
-          {/*      </li>*/}
+          {
+            navOptions.map((option, index) => (
+              <a key={index} href={option.link} className='font-bold text-base leading-5 text-center mr-10 cursor-pointer'>{option.name}</a>
+            ))
+          }
         </nav>
-        {/*  <div className="header__toggle">*/}
-        {/*    <i className="bx bx-menu"></i>*/}
-        {/*  </div>*/}
+        <div className='flex border-2 rounded-xl border-[#6b611f] cursor-pointer'>
+          {
+            theme === 'dark' ?
+              <Image src={sunSVG} alt={'sun'} width={25} className='mx-1' onClick={() => setTheme('light')}/>
+              : <Image src={moonSVG} alt={'moon'} width={25} className='mx-1' onClick={() => setTheme('dark')}/>
+          }
+        </div>
       </div>
     </div>
   )
