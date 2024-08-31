75fcda969cba92b22159dacf218f86e71be7c59c
gustavogomez <gomezf09@gmail.com>
2024-08-31 00:03:31 -0500

    improve SEO and metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,69 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
 import "./globals.css";
 import Header from "@/app/_sections/Header";
-import { ThemeProvider } from 'next-themes'
-const inter = Inter({ subsets: ["latin"] });
+import {ThemeProvider} from 'next-themes'
+
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
   title: "Gustavo Gomez | Software Engineer",
   description: "Desarrollo web y mobile",
+  keywords: ["software engineer", "web development", "mobile development", 'programacion', 'ingeniero de software', 'desarrollo web', 'desarrollo mobile'],
+  openGraph: {
+    title: "Gustavo Gomez | Software Engineer",
+    description: "Desarrollo personalizado de software web y mobile.",
+    images: [
+      {
+        url: 'https://stg.gustavogomez.dev/favicon.ico',
+      }
+    ],
+    url: 'https://stg.gustavogomez.dev/'
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Gustavo Gomez | Software Engineer",
+    description: "Desarrollo personalizado de software web y mobile.",
+    images: [
+      {
+        url: 'https://stg.gustavogomez.dev/favicon.ico',
+      },
+    ],
+  },
+  applicationName: 'Gustavo Gomez Portfolio',
+  robots: 'index, follow',
+  icons: [
+    {
+      url: '/favicon.ico',
+      sizes: '32x32',
+      type: 'icon',
+    },
+    {
+      url: '/favicon.ico',
+      sizes: '192x192',
+      type: 'icon',
+    },
+    {
+      url: '/favicon.ico',
+      sizes: '512x512',
+      type: 'icon',
+    }
+  ],
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className} font-sectionTitle`}>
-      <ThemeProvider attribute="class" defaultTheme="dark">
-        <Header />
-        {children}
-      </ThemeProvider>
-      </body>
+    <body className={`${inter.className} font-sectionTitle`}>
+    <ThemeProvider attribute="class" defaultTheme="dark">
+      <Header/>
+      {children}
+    </ThemeProvider>
+    </body>
     </html>
   );
 }
