3540803c6096d256504c3511a6b858b090b055c4
gustavogomez <gomezf09@gmail.com>
2024-09-16 14:00:51 -0500

    big improve to SEO, only for prod env

---
+++ src/app/sitemap.ts
@@ -0,0 +1,21 @@
+import type {MetadataRoute} from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap | null {
+  if (process.env?.SERVICE_ENV !== 'PROD') {
+    return null;
+  }
+
+  return [
+    {
+      url: 'https://gustavogomez.dev',
+      changeFrequency: 'daily',
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          es: 'https://gustavogomez.dev/es',
+          en: 'https://gustavogomez.dev/en',
+        },
+      },
+    },
+  ]
+}
